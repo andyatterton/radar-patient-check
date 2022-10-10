a78b0ee5deda7096fea7dc6e62f498ca0a562d1f
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-10-10 12:55:02 +0100

    Create a database session fastapi dependency

--- radar_patient_check/database.py
+++ radar_patient_check/database.py
@@ -1,8 +1,11 @@
-import os
+from sqlmodel import create_engine, Session
+from .config import settings
 
-from dotenv import find_dotenv, load_dotenv
-from sqlmodel import create_engine
 
-load_dotenv(find_dotenv())
+def get_session():
+    db_url = settings.sqlalchemy_database_url
+    if not db_url:
+        raise ValueError("SQLALCHEMY_DATABASE_URL not set")
 
-ukrdc_engine = create_engine(os.getenv("SQLALCHEMY_DATABASE_URL"))
+    with Session(create_engine(db_url)) as session:
+        yield session
