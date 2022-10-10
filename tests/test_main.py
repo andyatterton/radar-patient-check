4d6c2eb7a997670590b89485faef8f4c20e0a3d1
Joel Collins <joel.collins@renalregistry.nhs.uk>
2022-10-10 12:55:33 +0100

    Add a self-contained test database and add test_main_radar_check_dob_correct_nhs_not test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,102 +1,179 @@
-import os
+import datetime
 
-from dotenv import find_dotenv, load_dotenv
+import pytest
 from fastapi.testclient import TestClient
-
+from radar_patient_check.config import settings
+from radar_patient_check.database import get_session
 from radar_patient_check.main import app
-
-load_dotenv(find_dotenv())
-
-client = TestClient(app)
-
-fake_key = os.getenv("FAKEKEY")
-fake_identifier = os.getenv("FAKEIDENTIFIER")
-fake_date = os.getenv("FAKEDATE")
+from sqlmodel import Session, create_engine
+from sqlmodel.pool import StaticPool
+from ukrdc_sqla.ukrdc import Base as UKRDC3Base
+from ukrdc_sqla.ukrdc import Patient, PatientNumber, ProgramMembership
 
 
-def test_main_radar_check():
-    responce = client.get(
-        f"/radar_check/?nhs_number={fake_identifier}&dob={fake_date}",
-        headers={
-            "Authorization": f"Bearer {fake_key}",
-        },
+def _create_test_data(session: Session):
+    # Test patient with RADAR membership
+    session.add(
+        Patient(
+            pid=1,
+            birth_time=datetime.datetime(2000, 1, 1),
+        )
     )
-    assert responce.status_code == 200
-    assert responce.json() == {"nhs_number": True, "dob": True}
 
-
-def test_main_radar_check_no_patient():
-    responce = client.get(
-        "/radar_check/?nhs_number=6&dob=1900-01-01",
-        headers={
-            "Authorization": f"Bearer {fake_key}",
-        },
+    session.add(
+        PatientNumber(
+            id=1, pid=1, patientid="8888888888", numbertype="NI", organization="NHS"
+        )
     )
-    assert responce.status_code == 200
-    assert responce.json() == {"nhs_number": False, "dob": False}
 
-
-def test_main_radar_check_nhs_correct_dob_not():
-    responce = client.get(
-        f"/radar_check/?nhs_number={fake_identifier}&dob=1900-01-01",
-        headers={
-            "Authorization": f"Bearer {fake_key}",
-        },
+    session.add(
+        ProgramMembership(
+            id=1,
+            pid=1,
+            program_name="RADAR",
+            to_time=None,
+        )
     )
-    assert responce.status_code == 200
-    assert responce.json() == {"nhs_number": True, "dob": False}
 
+    # Test patient without RADAR membership
 
-def test_main_radar_check_nhs_correct_dob_missing():
-    responce = client.get(
-        f"/radar_check/?nhs_number={fake_identifier}",
-        headers={
-            "Authorization": f"Bearer {fake_key}",
-        },
+    session.add(
+        Patient(
+            pid=2,
+            birth_time=datetime.datetime(2001, 1, 1),
+        )
     )
-    assert responce.status_code == 422
-    assert responce.json() == {
-        "detail": [
-            {
-                "loc": ["query", "dob"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            },
-        ]
-    }
+
+    session.add(
+        PatientNumber(
+            id=2, pid=2, patientid="9999999999", numbertype="NI", organization="NHS"
+        )
+    )
+    session.commit()
 
 
-def test_main_radar_check_no_token():
-    response = client.get("/radar_check/?nhs_number=6&dob=1900-01-01")
-    assert response.status_code == 401
-    assert response.json() == {"detail": "Not authenticated"}
+@pytest.fixture(name="session")
+def session_fixture():
+    """
+    Create an in-memory test database and return a session.
+    """
+    engine = create_engine(
+        "sqlite://",
+        connect_args={"check_same_thread": False},
+        poolclass=StaticPool,
+    )
+    UKRDC3Base.metadata.tables["patient"].create(bind=engine)
+    UKRDC3Base.metadata.tables["patientnumber"].create(bind=engine)
+    UKRDC3Base.metadata.tables["programmembership"].create(bind=engine)
+
+    with Session(engine) as session:
+        _create_test_data(session)
+        yield session
 
 
-def test_main_radar_check_no_parameters():
-    responce = client.get(
+@pytest.fixture(name="client")
+def client_fixture(session: Session):
+    """
+    Create a test client and override the FastAPI dependency.
+    Depends on the session fixture.
+    """
+    settings.apikeys = ["PYTESTKEY0000000000"]
+
+    def get_session_override():
+        return session
+
+    app.dependency_overrides[get_session] = get_session_override
+
+    client = TestClient(app)
+    yield client
+    app.dependency_overrides.clear()
+
+
+def test_main_radar_check(client: TestClient):
+    response = client.post(
         "/radar_check/",
-        headers={
-            "Authorization": f"Bearer {fake_key}",
+        headers={"Authorization": "Bearer PYTESTKEY0000000000"},
+        json={
+            "nhsNumber": "8888888888",
+            "dateOfBirth": "2000-01-01",
         },
     )
-    assert responce.status_code == 422
-    assert responce.json() == {
-        "detail": [
-            {
-                "loc": ["query", "nhs_number"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            },
-            {
-                "loc": ["query", "dob"],
-                "msg": "field required",
-                "type": "value_error.missing",
-            },
-        ]
-    }
+    assert response.status_code == 200
+    assert response.json() == {"nhsNumber": True, "dateOfBirth": True}
 
 
-def test_main_radar_check_no_parameters_no_token():
-    responce = client.get("/radar_check/")
-    assert responce.status_code == 401
-    assert responce.json() == {"detail": "Not authenticated"}
+def test_main_radar_check_no_patient(client: TestClient):
+    response = client.post(
+        "/radar_check/",
+        headers={"Authorization": "Bearer PYTESTKEY0000000000"},
+        json={
+            "nhsNumber": "6",
+            "dateOfBirth": "1900-01-01",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {"nhsNumber": False, "dateOfBirth": False}
+
+
+def test_main_radar_check_nhs_correct_dob_not(client: TestClient):
+    response = client.post(
+        "/radar_check/",
+        headers={"Authorization": "Bearer PYTESTKEY0000000000"},
+        json={
+            "nhsNumber": "8888888888",
+            "dateOfBirth": "1900-01-01",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {"nhsNumber": True, "dateOfBirth": False}
+
+
+def test_main_radar_check_dob_correct_nhs_not(client: TestClient):
+    # We need to make sure that any non-RADAR patients do not have their DoB "leaked" to the client.
+    #   E.g. if a valid NHS number is given, but has no RADAR membership,
+    #   the client should not be able to determine if the DoB was valid or not.
+
+    response = client.post(
+        "/radar_check/",
+        headers={"Authorization": "Bearer PYTESTKEY0000000000"},
+        json={
+            "nhsNumber": "9999999999",
+            "dateOfBirth": "2001-01-01",
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {"nhsNumber": False, "dateOfBirth": False}
+
+
+def test_main_radar_check_nhs_correct_dob_missing(client: TestClient):
+    response = client.post(
+        "/radar_check/",
+        headers={"Authorization": "Bearer PYTESTKEY0000000000"},
+        json={
+            "nhsNumber": "8888888888",
+        },
+    )
+    assert response.status_code == 422
+
+
+def test_main_radar_check_no_token(client: TestClient):
+    response = client.post(
+        "/radar_check/",
+        json={
+            "nhsNumber": "8888888888",
+            "dateOfBirth": "2000-01-01",
+        },
+    )
+    assert response.status_code == 401
+
+
+def test_main_radar_check_bad_token(client: TestClient):
+    response = client.post(
+        "/radar_check/",
+        headers={"Authorization": "Bearer BADTOKEN"},
+        json={
+            "nhsNumber": "8888888888",
+            "dateOfBirth": "2000-01-01",
+        },
+    )
+    assert response.status_code == 401
